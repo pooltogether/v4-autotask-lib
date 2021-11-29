5c05f006753961b22829e2822dcdf801ffb57ea7
Kames Geraghty <kamescg@gmail.com>
2021-11-29 14:14:47 +0000

    update(L2DrawAndPrizeDistributionPush): define multiple state of beacon/receiver chains when propagating draws

--- src/L2DrawAndPrizeDistributionPush.ts
+++ src/L2DrawAndPrizeDistributionPush.ts
@@ -1,7 +1,7 @@
 
 import { BigNumber } from '@ethersproject/bignumber';
 import { Contract } from '@ethersproject/contracts';
-import { ActionState, CalculateL2DrawAndPrizeDistributionConfig, ContractsBlob, Relayer } from './types'
+import { ActionState, CalculateL2DrawAndPrizeDistributionConfig, ContractsBlob, Draw, Relayer } from './types'
 import { getContract } from './get/getContract';
 import { getJsonRpcProvider } from "./get/getJsonRpcProvider";
 import { computePrizeDistributionFromTicketAverageTotalSupplies, getMultiTicketAverageTotalSuppliesBetween } from './utils'
@@ -77,39 +77,78 @@ export async function L2DrawAndPrizeDistributionPush(
       const { drawId: drawIdOldestFromBeaconChain } = await prizeDistributionBufferBeaconChain.getOldestPrizeDistribution()
       oldestBeaconChainDrawId = drawIdOldestFromBeaconChain
     }
+    debug(`ReceiverChain: Newest PrizeDistribution Draw ID is ${lastPrizeDistributionDrawId}`)
+    debug(lastPrizeDistributionDrawId)
+    debug(drawNewest.drawId)
+
+    const timelockElapsed = await drawCalculatorTimelockReceiverChain.hasElapsed()
+    debug(timelockElapsed)
+
+    if (!timelockElapsed) {
+      // IF the timelock has not elapsed, we can NOT push a new Draw/PrizeDistribution so we return early.
+      throw new Error('DrawCalculatorTimelockReceiverChain/timelock-not-elapsed')
+    }
 
     /**
      * Depending on the state of the Beacon and Receiver chain, existing prize distributions may NOT required on the receiver chain.
-     * State 0: The Beacon chain has not yet been initialized.
-     * State 1: Beacon Chain has N draws and started at DrawId 1 and Receiver Chain has N draws and started at DrawId 1
-     * State 2: Beacon Chain has N draws and started at DrawId 1 receiver Chain has 0 draws and should start at newest Beacon Chain DrawId
+     * State 0: The Beacon and Receiver chains have not been initialized.
+     * State 1: Beacon Chain is 1 draw ahead of the Receiver chain.
+     * State 2: Beacon Chain is 2 draws ahead of an uninitialized Receiver chain.
+     * State 3: Beacon Chain is 2 draws ahead of an initialized Receiver chain.
      */
 
     if (oldestBeaconChainDrawId === 0 && newestBeaconChainDrawId === 0) {
       throw new Error('BeaconChainPrizeDistributionBuffer/no-prize-distribution-buffer-available')
     }
 
-    // IF the Receiver chain is 1 draw behind the Beacon chain, we need to trigger a prize distribution.
+    let drawIdToFetch = 0;
+    let drawFromBeaconChainToPush: Draw | undefined;
+
     if (newestBeaconChainDrawId === newestReceiverChainDrawId + 1) {
-      // TODO: apply State 1 logic
+      /**
+       * IF the Beacon chain has exactly 1 more draw and than Receiver chain we can
+       * PUSH the NEWEST draw from the Beacon chain to the Receiver chain.
+       * @dev This includes the case where the Receiver chain has 0 draws and the Beacon chain has 1 draws.
+       * @dev This includes the case where the Receiver chain has 1 draw and the Beacon chain has 2 draws.
+       * @dev Both when the Receiver chain is being initialize and when the Receiver is staying in sync with the Beacon chain
+       *      we can request the newest draw from the Beacon chain.
+       */
+      drawIdToFetch = newestBeaconChainDrawId
+      drawFromBeaconChainToPush = await drawBufferBeaconChain.getDraw(drawIdToFetch)
     }
 
-    if (oldestBeaconChainDrawId >= 1 && newestReceiverChainDrawId === 0) {
-      // TODO: apply State 2 logic 
+    if (newestBeaconChainDrawId > 1 && newestReceiverChainDrawId === 0) {
+      /**
+       * IF the Beacon chain has 2 or more draws and the Receiver chain is NOT initialized, we can
+       * PUSH the NEWEST draw from the Beacon chain to the Receiver chain.
+       * @dev This scenario is likely to occur if a new PrizePool is created on the Receiver chain after a Beacon chain has
+       *      been previously initialized. We're assuming the Receiver chain has no need to sync with previous Beacon chain Draws and PrizeDistributions, 
+       *      because the receiver chain PrizePool DID NOT exist yet when those parameters were created.
+       */
+      drawIdToFetch = newestBeaconChainDrawId
+      drawFromBeaconChainToPush = await drawBufferBeaconChain.getDraw(drawIdToFetch)
     }
 
+    if (newestBeaconChainDrawId > newestReceiverChainDrawId + 1) {
+      /**
+       * IF the Beacon chain is 2 draws ahead of the Receiver chain AFTER the Receiver has been initialized, we can
+       * PUSH a Draw between the OLDEST and NEWEST from the Beacon chain to the Receiver chain.
+       * @dev This scenario is likely to occur if the Receiver chain missed a draw from the Beacon chain
+       *      and needs to catch up to Beacon chain, since it was previously in sync.
+       */
+      drawIdToFetch = newestReceiverChainDrawId + 1
+      drawFromBeaconChainToPush = await drawBufferBeaconChain.getDraw(drawIdToFetch)
+    }
 
-    const timelockElapsed = await drawCalculatorTimelockReceiverChain.hasElapsed()
-    debug(`Last ReciverChain PrizeDistribution Draw ID is ${lastPrizeDistributionDrawId}`)
-    debug(lastPrizeDistributionDrawId)
-    debug(drawNewest.drawId)
-    debug(timelockElapsed)
+    // WHY would we get undefined? What situation are missing?
+    if (typeof drawFromBeaconChainToPush === 'undefined') {
+      throw new Error('DrawBufferBeaconChain/error-calculating-correct-draw')
+    }
 
-    // If the prize distribution hasn't propagated and we're allowed to push
-    const drawId = lastPrizeDistributionDrawId + 1;
-    const draw = await drawBufferBeaconChain.getDraw(drawId)
-    debug("DrawId: ", drawId)
+    const draw = drawFromBeaconChainToPush;
+    debug("Fetched Draw ID: ", drawIdToFetch)
     debug("Draw: ", draw)
+
     const prizeTier = await prizeTierHistoryBeaconChain.getPrizeTier(draw.drawId)
     const endTimestampOffset = prizeTier.endTimestampOffset
     const startTimestampOffset = draw.beaconPeriodSeconds
@@ -125,20 +164,27 @@ export async function L2DrawAndPrizeDistributionPush(
 
     if (totalSupplyOtherTickets && !totalSupplyOtherTickets[0]) throw new Error('No totalSupplyOtherTickets')
 
-    // @ts-ignore
-    const prizeDistribution = await computePrizeDistributionFromTicketAverageTotalSupplies(draw, prizeTier, BigNumber.from(L2TicketTotalSupply[0]), [totalSupplyOtherTickets[0]], decimals)
+    const prizeDistribution = await computePrizeDistributionFromTicketAverageTotalSupplies(
+      draw,
+      prizeTier,
+      BigNumber.from(L2TicketTotalSupply[0]),
+      // @ts-ignore
+      [totalSupplyOtherTickets[0]],
+      decimals
+    )
     debug("PrizeDistribution: ", prizeDistribution)
     if (!prizeDistribution) throw new Error('PrizeDistribution is undefined')
 
     debug("prizeDistribution: ", prizeDistribution)
     debug('prizeDistribution:prize', prizeDistribution.prize.toString())
+
     if (lastPrizeDistributionDrawId < drawNewest.drawId && timelockElapsed) {
       tx = await L2TimelockTriggerReceiverChain.populateTransaction.push(draw, prizeDistribution)
       if (config.execute && relayer) {
-        debug(`Pushing L2 prize distrubtion for draw ${drawId}...`)
+        debug(`Pushing L2 prize distrubtion for draw ${drawIdToFetch}...`)
         txRes = await relayer.sendTransaction({
           data: tx.data,
-          to: draw.address,
+          to: tx.to,
           speed: 'fast',
           gasLimit: 500000,
         });
@@ -155,7 +201,7 @@ export async function L2DrawAndPrizeDistributionPush(
       status,
       response,
       data: {
-        drawBufferDrawNewest: drawNewest,
+        // drawBufferDrawNewest: drawNewest,
       },
       transaction: {
         data: tx?.data,
