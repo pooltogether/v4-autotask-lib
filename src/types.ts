d145e51f702c3c018f0c739b7cea67887d33df6f
Kames Geraghty <3408362+kamescg@users.noreply.github.com>
2021-12-15 16:49:50 -0500

    Update src/types.ts
    Co-authored-by: Pierrick Turelier <pierrick@turelier.com>

--- src/types.ts
+++ src/types.ts
@@ -15,7 +15,7 @@ export interface Config {
   chainId: number;
   network: string;
   apiKey: string | undefined;
-  speed?: 'slow' | 'normal' | 'fast' | string;
+  speed?: 'slow' | 'normal' | 'fast';
   gasLimit?: number | string;
   execute?: Boolean;
 }
