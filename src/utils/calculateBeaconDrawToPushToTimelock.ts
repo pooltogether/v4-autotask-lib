5a43d061b3f2d53c53928c0c255a80990190d153
kames <kamescg@gmail.com>
2021-12-18 07:04:30 -0700

    update(calculateBeaconDrawToPushToTimelock): add notes and verify correctness

--- src/utils/calculateBeaconDrawToPushToTimelock.ts
+++ src/utils/calculateBeaconDrawToPushToTimelock.ts
@@ -1,29 +1,39 @@
 import { Contract } from '@ethersproject/contracts';
+import { Draw } from '../types';
 
 export async function calculateBeaconDrawToPushToTimelock(
   drawBufferBeaconChain: Contract,
   prizeDistributionBufferBeaconChain: Contract
 ) {
-  let drawIdToFetch;
-  let drawNewestFromBeaconChain;
+  let drawNewestFromBeaconChain: Draw;
+  let drawIdToFetch: number = 0;
+  let newestPrizeDistributionDrawId: number = 0;
   let lockAndPush: Boolean = false;
-  let newestPrizeDistributionDrawId = 0;
+
+  /**
+   * Fetch Draw newest from Beacon Chain
+   * IF the fetch throws an error, the DrawBuffer is not initialized
+   */
   try {
     drawNewestFromBeaconChain = await drawBufferBeaconChain.getNewestDraw();
   } catch (error) {
     throw new Error('BeaconChain: DrawBuffer is not initialized');
   }
+
+  /**
+   * Fetch PrizeDistribution newest from Beacon Chain
+   * IF the fetch throws an error, the PrizeDistribution is not initialized
+   */
   try {
-    const {
-      drawId,
-    } = await prizeDistributionBufferBeaconChain.getNewestPrizeDistribution();
-    if (drawId > 0) {
-      newestPrizeDistributionDrawId = drawId;
-    }
+    const { drawId } = await prizeDistributionBufferBeaconChain.getNewestPrizeDistribution();
+    newestPrizeDistributionDrawId = drawId;
   } catch (error) {
     newestPrizeDistributionDrawId = 0;
   }
 
+  /**
+   * IF the PrizeDistribution is behind the DrawBuffer
+   */
   if (newestPrizeDistributionDrawId < drawNewestFromBeaconChain.drawId) {
     lockAndPush = true;
     drawIdToFetch = newestPrizeDistributionDrawId + 1;
@@ -33,7 +43,7 @@ export async function calculateBeaconDrawToPushToTimelock(
       newestPrizeDistributionDrawId
     );
   }
-
+  
   return {
     lockAndPush,
     drawIdToFetch,
