b579209c074dcdc3a6a750a6f0b282908123f888
kames <kamescg@gmail.com>
2022-01-07 09:17:29 -0700

    add tests

---
+++ test/exports.test.ts
@@ -0,0 +1,9 @@
+import { drawBeaconHandleDrawStartAndComplete, beaconDrawLockAndNetworkTotalSupplyPush, receiverDrawLockAndNetworkTotalSupplyPush } from '../src';
+
+describe('exports', () => {
+  it('should have all expected exports defined', () => {
+    expect(drawBeaconHandleDrawStartAndComplete).toBeDefined();
+    expect(beaconDrawLockAndNetworkTotalSupplyPush).toBeDefined();
+    expect(receiverDrawLockAndNetworkTotalSupplyPush).toBeDefined();
+  });
+});
